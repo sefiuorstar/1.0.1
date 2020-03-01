ddbad235953bd51138342dd38ea0494298122437
sefiuorstar <39660202+sefiuorstar@users.noreply.github.com>
2020-03-01 19:01:25 +0100

    Update elclassico.php

--- elclassico.php
+++ elclassico.php
@@ -19,3 +19,12 @@ http://dreamsat.iksa.cc:2086/live/rediptv0dreamsat/XP847wvHuv/2797.m3u8"
 
 
 "http://134.209.100.165:8080/hls/ch1.m3u8"
+
+
+
+"http://34.107.131.44/live/l/chunks.m3u8"
+
+
+"http://push13.feber.xyz/hls/channel21hd47/chunks.m3u8"
+
+
