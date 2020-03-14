863879dac706d84eaae98aeebaf3a3b0fafe87fc
sefiuorstar <39660202+sefiuorstar@users.noreply.github.com>
2020-03-14 12:24:59 +0100

    Update baslat.php

--- baslat.php
+++ baslat.php
@@ -1,7 +1,14 @@
 
 {"DeviceNo":"921486","DeviceHash":"4e90cf2f938d242eb10ff3e680b1211f712bdfbf","updateStatus":true,
-"updateMessage":"A new version is available. Please download the new version and enjoy the sports channels
-➡️ Here</a></strong></span>.</div> </div> <ul> <li>More than 1000 TV channels supported</li> <li>Support lots of countries TV channels like USA, UK, Saudi Arabia, India, Pakistan, Bangladesh, France, Italy, Canada, Portugal, etc.</li> <li>Well categorized genres, such as Sports, Religion, News, Movies, Entertainment, etc.</li> <li>AOS TV has Popular Trending Topics such as EPL, Cricket, Tennis, etc.</li> <li>AOS TV compatible with Android supported devices such as Android TV Box, Android Smartphone, Amazon Firestick, Fire TV, NVIDIA SHIELD, etc.</li> </ul> <p>Related App:</p> <ul> <li><a  ",
+"updateMessage":"A new version is available. Please download the new version and enjoy More than 1000 TV channels supported
+Support lots of countries TV channels like USA, UK, Saudi Arabia, India, Pakistan, Bangladesh, France, Italy, Canada, Portugal, etc.
+Well categorized genres, such as Sports, Religion, News, Movies, Entertainment, etc.
+AOS TV has Popular Trending Topics such as EPL, Cricket, Tennis, etc.
+AOS TV compatible with Android supported devices such as Android TV Box, Android Smartphone, Amazon Firestick, Fire TV, NVIDIA SHIELD, etc.
+
+
+
+  ",
 "updateServer":"https:\/\/play.google.com\/store\/apps\/details?id=com.mobilcanlitvizle.app&hl=tr","AdsCount":4,
 "AdMobPublisherId":"pub-","AdMobBannerAds":true,
 "AdMobBannerAdUnitId":"ca-app-pub-\/","AdMobInterstitialAds":true,
