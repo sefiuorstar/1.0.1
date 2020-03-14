9016c9895f2dcde8c0c9c2c6d0168a320a78dfa8
sefiuorstar <39660202+sefiuorstar@users.noreply.github.com>
2020-03-14 11:51:32 +0100

    Update baslat.php

--- baslat.php
+++ baslat.php
@@ -1,5 +1,5 @@
 
-{"DeviceNo":"921486","DeviceHash":"4e90cf2f938d242eb10ff3e680b1211f712bdfbf","updateStatus":false,
+{"DeviceNo":"921486","DeviceHash":"4e90cf2f938d242eb10ff3e680b1211f712bdfbf","updateStatus":true,
 "updateMessage":"A new version is available. Please download the new version and enjoy the sports channels",
 "updateServer":"https:\/\/play.google.com\/store\/apps\/details?id=com.mobilcanlitvizle.app&hl=tr","AdsCount":4,
 "AdMobPublisherId":"pub-","AdMobBannerAds":true,
