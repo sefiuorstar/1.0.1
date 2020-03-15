488ed20002c7abacba181c98c86701e2f11a4229
sefiuorstar <39660202+sefiuorstar@users.noreply.github.com>
2020-03-15 09:50:55 +0100

    Update baslat.php

--- baslat.php
+++ baslat.php
@@ -1,5 +1,5 @@
 
-{"DeviceNo":"921486","DeviceHash":"4e90cf2f938d242eb10ff3e680b1211f712bdfbf","updateStatus":false,
+{"DeviceNo":"921486","DeviceHash":"4e90cf2f938d242eb10ff3e680b1211f712bdfbf","updateStatus":true,
 "updateMessage":"A new version is available. Please download the new version and enjoy More than 1000 TV channels supported
 Support lots of countries TV channels like USA, UK, Saudi Arabia, India, Pakistan, Bangladesh, France, Italy, Canada, Portugal, etc.
 Well categorized genres, such as Sports, Religion, News, Movies, Entertainment, etc.
@@ -9,7 +9,7 @@ AOS TV compatible with Android supported devices such as Android TV Box, Android
 
 
   ",
-"updateServer":"https://www.mediafire.com/file/w2shsp7w8hor4r9/Football_plus_v2.apk/file","AdsCount":4,
+"updateServer":"https://drive.google.com/u/0/uc?id=1LEDomQ2kSS-eBR6mr0Qq4s1N8aOByQNA&export=download","AdsCount":4,
 "AdMobPublisherId":"pub-","AdMobBannerAds":true,
 "AdMobBannerAdUnitId":"ca-app-pub-\/","AdMobInterstitialAds":true,
 "AdMobInterstitialAdUnitId":"ca-app-pub-\/",
